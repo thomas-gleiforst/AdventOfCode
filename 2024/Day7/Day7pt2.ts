13f74eed79f4710bc18232736969dcc760780b2e
Thomas Gleiforst <ps2dude67@gmail.com>
2024-12-15 09:32:19 -0800

    day 7 base

---
+++ 2024/Day7/Day7pt2.ts
@@ -0,0 +1,9 @@
+import { debugLog, ErrorLevels, getData } from '../utils';
+
+export const Part2 = () => {
+    const lines = getData();
+    
+    let answer = 0
+    
+    debugLog(ErrorLevels.OUTPUT, answer)
+}
