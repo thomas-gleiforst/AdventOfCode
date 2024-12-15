a587d20ee783d2c6291e9a6a122124d04e05f9ee
Thomas Gleiforst <ps2dude67@gmail.com>
2024-12-15 09:57:25 -0800

    Day 7

--- 2024/Day7/Day7.ts
+++ 2024/Day7/Day7.ts
@@ -1,9 +1,47 @@
+import { availableMemory } from 'process';
 import { debugLog, ErrorLevels, getData } from '../utils';
 
 export const Part1 = () => {
     const lines = getData();
     
     let answer = 0
+
+    type Operator = ("*" | "+")
+    const operators: Operator[]  = ["*", "+"]
+
+    const doOp = (op: Operator, left: number, right: number) => {
+        switch (op) {
+            case "*": return left * right;
+            case "+": return left + right;
+        }
+    }
+
+
+    for (let line of lines) {
+        const [targetString, eqString] = line.split(": ")
+        const target = parseInt(targetString)
+        const sequence = eqString.split(" ").map(i => parseInt(i))
+
+        
+        const tryOps = (sequence: number[]): number | false => {
+            if (sequence.length === 1) return sequence[0]
+
+            const [first, second, ...rest] = sequence
+
+            for (const op of operators) {
+                let result = doOp(op, first, second)
+                if (target === tryOps([result, ...rest])) {
+                    return target
+                }
+            }
+            return false
+        }
+
+        const attempt = tryOps(sequence)
+        if (attempt) {
+            answer += attempt
+        }
+    }
     
     debugLog(ErrorLevels.OUTPUT, answer)
 }
