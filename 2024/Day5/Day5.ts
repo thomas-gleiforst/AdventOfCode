c79155a59789a67f4cd5cff8bc55dff0a3fbddcc
Thomas Gleiforst <ps2dude67@gmail.com>
2024-12-15 09:31:56 -0800

    day 5

--- 2024/Day5/Day5.ts
+++ 2024/Day5/Day5.ts
@@ -18,35 +18,70 @@ export const Part1 = () => {
 
     debugLog(ErrorLevels.DEBUG, ordering)
 
-    const checkPagesAfter = (num: number, line: number[]) => {
-        if (!ordering?.[num]?.length || !line.length) return true
-        return !!line.find(page => ordering?.[num]?.includes(page))
+    const pagesAfterOkay = (num: number, line: number[]) => {
+        if (!ordering?.[num]?.length) {
+            debugLog(ErrorLevels.DEBUG, `Pages after ${num} valid, as ${line} have no orderings for it`)
+            return true
+        }
+        if (!line.length) {
+            debugLog(ErrorLevels.DEBUG, `Pages after ${num} valid, as ${line} is empty`)
+            return true
+        }
+
+        const issueFound = line.find(page => {
+            const currPageIsEntryForNum = ordering?.[page]?.includes(num);
+            currPageIsEntryForNum && debugLog(ErrorLevels.DEBUG, `Pages before ${num} invalid as ${ordering?.[page]} includes ${num}`)
+            
+            return currPageIsEntryForNum
+        })
+        issueFound && debugLog(ErrorLevels.DEBUG, `Pages after ${num} (${line}) rejected due to later entry: ${issueFound}`)
+
+        return !issueFound
     }
 
-    const checkPagesBefore = (num: number, line: number[]) => {
-        if (!ordering?.[num]?.length || !line.length) return true
-        return !!line.find(page => {
+    const pagesBeforeOkay = (num: number, line: number[]) => {
+        if (!ordering?.[num]?.length) {
+            debugLog(ErrorLevels.DEBUG, `Pages before ${num} valid, as ${line} have no orderings for it`)
+            return true
+        }
+        if (!line.length) {
+            debugLog(ErrorLevels.DEBUG, `Pages before ${num} valid, as ${line} is empty`)
+            return true
+        }
+        const issueFound = line.find(page => {
             debugLog(ErrorLevels.VERBOSE, page, num, !ordering?.[num]?.includes(page), ordering?.[page]?.includes(num))
-            return ordering?.[num]?.includes(page) && !ordering?.[page]?.includes(num)
+            
+            const currNumHasEntryForPriorPage = ordering?.[num]?.includes(page)
+            currNumHasEntryForPriorPage && debugLog(ErrorLevels.DEBUG, `${num} invalid as ${ordering?.[num]} includes ${page}`)
+            
+            return currNumHasEntryForPriorPage
         })
+        issueFound && debugLog(ErrorLevels.DEBUG, `Pages before ${num} (${line}) rejected due to prior entry: ${issueFound}`)
+        return !issueFound 
     }
 
     let answer = 0
     
-    update.forEach(book => {
+    update.forEach((book, bookIndex) => {
         let right_order = true
 
         book.forEach((page, index) => {
+            const pagesAfterFailed = !pagesAfterOkay(page, book.slice(index+1))
+            const pagesBeforeFailed = !pagesBeforeOkay(page, book.slice(0, index))
             if (
-                !checkPagesAfter(page, book.slice(index+1)) ||
-                !checkPagesBefore(page, book.slice(0, index))
-            ) right_order = false
-            debugLog(ErrorLevels.VERBOSE, page, checkPagesAfter(page, book.slice(index+1)), checkPagesBefore(page, book.slice(0, index)))
+                pagesAfterFailed || pagesBeforeFailed
+            ) {
+                debugLog(ErrorLevels.INFO, `${page} in ${book} not in correct order${pagesAfterFailed ? " - pages after failed" : ""}${pagesBeforeFailed ? " - pages before failed" : ""}`)
+                right_order = false
+            }
+
+            debugLog(ErrorLevels.VERBOSE, page, pagesAfterFailed, pagesBeforeFailed)
         })
 
-        if (right_order) answer += book[Math.floor(book.length/2)]
+        const bookScore = book[Math.floor(book.length/2)]
+        if (right_order) answer += bookScore
 
-        debugLog(ErrorLevels.DEBUG, right_order, book[Math.floor(book.length/2)])
+        debugLog(ErrorLevels.DEBUG, `Book #${bookIndex+1}: ${book} is in the ${right_order ? "right order" : "wrong order"}, with a new score ${answer} \n`)
     })
     
     debugLog(ErrorLevels.OUTPUT, answer)
